acfcb84b5cbca909d62e64f7e538d047289d1825
Victory <git@dfhu.org>
2017-07-25 18:36:34 -0400

    Add AbstractViewModel

---
+++ src/main/java/org/dfhu/sparkingrocks/viewmodels/AbstractViewModel.java
@@ -0,0 +1,8 @@
+package org.dfhu.sparkingrocks.viewmodels;
+
+import org.dfhu.sparkingrocks.session.VicSession;
+
+public class AbstractViewModel {
+  public AbstractViewModel(VicSession vicSession) {
+  }
+}
